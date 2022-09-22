cab02fc3e9de512e877abbc53d150407928f910c
rohith1222004 <brohithkumar02@gmail.com>
2022-09-22 13:40:07 +0530

    what to process page is added

---
+++ screens/what_Process.js
@@ -0,0 +1,68 @@
+import {StyleSheet, Text, View,Image } from 'react-native';
+import Btn from './Components/Btn';
+
+
+const What_Process = () =>{
+
+
+
+  return (
+    <View style={styles.container} >
+      <View style={styles.options}>
+        <Image source={require("./assets/options.png")}/>
+      </View>
+      <View style={styles.WelcomeWrap}>
+          <View style={styles.WelcomeText}>
+            <Text style={{fontSize:30,textAlign:'center'}}>What to process ?</Text>
+          </View>
+      </View>
+
+      <View style={styles.buttonWrap}>
+        <View style={styles.button1}>
+           <Btn content={"Upload/Scan new data"} />
+        </View>
+
+        <View style={styles.button2}>
+          <Btn content={"Access Health Records"} />
+        </View>
+
+      </View>
+     
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container:{
+    
+  },
+  WelcomeText:{
+    width:350,
+    marginTop:200,
+  },
+  WelcomeWrap:{
+    alignItems:'center',
+  },
+  button1:{
+    width:350,
+    height:50,
+
+  },
+  button2:{
+    width:350,
+    height:50,
+    marginTop:75
+  },
+  buttonWrap:{
+    alignItems:'center',
+    marginTop:85
+  },
+  options:{
+    marginTop:40,
+    marginLeft:10
+  }
+});
+
+export default What_Process;
